17a08b4407d8dbbdb2f2a28488fffad9ad9fe98b
414645 <414645@bsd48.org>
2023-05-23 13:13:59 -0700

    initialize

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+using namespace std;
+
+//by ryan veith
+//traveling salesman problem kina
+//Directed Weighted Graph thingie
+
+//void
+
+int main() {
+  cout << "hello world" << endl;
+}
